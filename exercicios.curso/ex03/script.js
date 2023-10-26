b61a9070fa0eadebd9cb2c9813cf7921e29d4ec6
João Guilherme Medeiros <devjoaogui@gmail.com>
2023-10-26 13:24:09 -0300

    ex03 - super contador

--- exercicios.curso/ex03/script.js
+++ exercicios.curso/ex03/script.js
@@ -2,8 +2,32 @@ function contar() {
     let inicio = document.getElementById('inicioId')
     let fim = document.getElementById('fimId')
     let passo = document.getElementById('passoId')
-    let contagem = document.querySelector('p#contagem')
-    let conta = document.querySelector('p#conta')
+    let contagem = document.getElementById('contagem')
 
-    
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        contagem.innerHTML = 'Impossível contar pois faltam dados!'
+    } else {
+        contagem.innerHTML = 'Contando... <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if (i < f) {
+            // contagem crescente
+            for(let c = i; c <= f; c += p) {
+                contagem.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            // contagem decrescente
+            for(let c = i; c >= f; c -= p) {
+                contagem.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        contagem.innerHTML += `\u{1F3C1}`
+    }
 }
