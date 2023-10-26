3dd6541e8421cfd4e6e5232be86a5647ab77af06
João Guilherme Medeiros <devjoaogui@gmail.com>
2023-10-26 12:59:20 -0300

    estrutura do ex03 criada

---
+++ exercicios.curso/ex03/script.js
@@ -0,0 +1,9 @@
+function contar() {
+    let inicio = document.getElementById('inicioId')
+    let fim = document.getElementById('fimId')
+    let passo = document.getElementById('passoId')
+    let contagem = document.querySelector('p#contagem')
+    let conta = document.querySelector('p#conta')
+
+    
+}
