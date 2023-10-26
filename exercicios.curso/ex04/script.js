68a0d657c5c4f3a49ddbbf4e25b96c7aeb65f018
João Guilherme Medeiros <devjoaogui@gmail.com>
2023-10-26 14:32:45 -0300

    add 'values' nos itens

--- exercicios.curso/ex04/script.js
+++ exercicios.curso/ex04/script.js
@@ -11,6 +11,7 @@ function tabuada() {
         for(c; c <= 10; c++) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}`
             tab.appendChild(item)
         }
     }
