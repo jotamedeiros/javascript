cabbcc8ef78249832afb35d19b0cfbf545d53d68
jotamedeiros <devjoaogui@gmail.com>
2023-12-19 15:30:45 -0300

    ex05 - estrutura inicial

---
+++ exercicios.curso/ex05/script.js
@@ -0,0 +1,9 @@
+let num = document.querySelector('input#fnum')
+let flista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function adicionar(n) {
+    if(isNumero(num.val))
+}
+
